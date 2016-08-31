b03c5dbf01f721f3746730433356b8a69a69552c
Bryan Hankins <bryanhankins@me.com>
2016-08-30 20:54:34 -0500

    logging submit button event listener

---
+++ cards.js
@@ -0,0 +1,8 @@
+var userType = document.getElementById("user-info");
+var submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener("click", function (event) {
+console.log("clicked submit button", event.submitBtn)
+
+})
+
