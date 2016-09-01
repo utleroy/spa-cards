c01e5176f5ff38bad4cd79462669de77983734fb
Bryan Hankins <bryanhankins@me.com>
2016-08-31 20:59:53 -0500

    added materialize layout for kicks

--- cards.js
+++ cards.js
@@ -5,23 +5,17 @@ var outToDom = document.getElementById("DOM");
 var remove = document.getElementsByClassName("delete");
 var addedCard = document.getElementsByClassName("addedCard");
 
-
-
 submitBtn.addEventListener("click", function newCard() {
-  var output = "<div class='addedCard'><section><p>" + userType.value + "</p></section><input type='button' class='delete' value='Delete'></div>"
-
+  var output = "<div class='addedCard'>" + '<div class="row"><div class="col s12 m6"><div class="card blue-grey darken-1"><div class="card-content white-text"><span class="card-title">New Card</span><p><section>' + userType.value + '</div></div></div></div>' + "</p></section><input type='button' class='btn delete' value='Delete'></div>"
   outToDom.innerHTML += output;
-
-
   console.log("new card", output)
+
   deleteButtonFunction()
 })
-
 function deleteFromDom (event) {
   var currentCard = event.target.parentElement;
   outToDom.removeChild(currentCard)
 }
-
 function deleteButtonFunction() {
   for (var i = 0; i < remove.length; i++) {
     var deleteLoop = document.getElementsByClassName("delete");
