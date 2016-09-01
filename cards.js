c741927a0922ec39509e9c919395a29393f08616
Bryan Hankins <bryanhankins@me.com>
2016-08-31 19:22:56 -0500

    card info displaying in the DOM

--- cards.js
+++ cards.js
@@ -1,8 +1,20 @@
-var userType = document.getElementById("user-info");
+var userType = document.getElementById("user-info")
 var submitBtn = document.getElementById("submit");
+var card = document.getElementById("addedCard");
+var remove = document.getElementsByClassName("delete");
+var outToDom = document.getElementById("DOM");
 
-submitBtn.addEventListener("click", function (event) {
-console.log("clicked submit button", event.submitBtn)
+
+
+submitBtn.addEventListener("click", function newCard() {
+  var output = "<div class='addedCard'><p>" + userType.value + "</p></div>"
+      output += "<button class='delete'>Remove</button>"
+      DOM.innerHTML += output;
+
+
+  console.log("new card", output)
 
 })
+
+
 
