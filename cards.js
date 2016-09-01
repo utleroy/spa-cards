8330322a47aef76bd974e72db3cb91954d757bdd
Bryan Hankins <bryanhankins@me.com>
2016-08-31 19:50:48 -0500

    requirements met

--- cards.js
+++ cards.js
@@ -1,20 +1,33 @@
 var userType = document.getElementById("user-info")
 var submitBtn = document.getElementById("submit");
-var card = document.getElementById("addedCard");
-var remove = document.getElementsByClassName("delete");
 var outToDom = document.getElementById("DOM");
+
+var remove = document.getElementsByClassName("delete");
+var addedCard = document.getElementsByClassName("addedCard");
 
 
 
 submitBtn.addEventListener("click", function newCard() {
-  var output = "<div class='addedCard'><p>" + userType.value + "</p></div>"
-      output += "<button class='delete'>Remove</button>"
-      DOM.innerHTML += output;
+  var output = "<div class='addedCard'><section><p>" + userType.value + "</p></section><input type='button' class='delete' value='Delete'></div>"
+
+  outToDom.innerHTML += output;
 
 
   console.log("new card", output)
-
+  deleteButtonFunction()
 })
 
+function deleteFromDom (event) {
+  var currentCard = event.target.parentElement;
+  outToDom.removeChild(currentCard)
+}
 
+function deleteButtonFunction() {
+  for (var i = 0; i < remove.length; i++) {
+    var deleteLoop = document.getElementsByClassName("delete");
+    for (var j = 0; j < deleteLoop.length; j++) {
+      deleteLoop[j].addEventListener("click", deleteFromDom)
+    }
+  }
+}
 
